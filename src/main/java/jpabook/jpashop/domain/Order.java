b7b850a610215742fe05d52b0ff9527447be8c42
TaegeunYou <66227661+TaegeunYou@users.noreply.github.com>
2021-07-17 06:53:09 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,54 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.FetchType.*;
+
+@Entity
+@Table(name = "ORDERS")
+@Getter @Setter
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ID")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    //모든 Entity는 저장하고 싶으면 각각 persist 해주어야 되는데 cascade를 붙이면 자동으로 persist 된다.
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "DELIVERY_ID")
+    private Delivery delivery;
+
+    private LocalDateTime orderDate;  //주문시간
+
+    @Enumerated
+    private OrderStatus status; //주문상태 [ORDER, CANCEL]
+
+    //==연관관계 메서드==//
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+}
---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,68 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true) // JPA의 데이터 변경은 transaction안에서 일어나야 된다. // readOnly는 성능 최적화 // private메서드에는 transactional이 적용되지 않는다.
+//@AllArgsConstructor   //모든 필드를 갖고 생성자를 만듦
+@RequiredArgsConstructor    //final 필드를 갖고 생성자를 만듦 //생성자가 하나일 경우 @Autowired 생략 가능
+public class MemberService {
+
+    //컴파일 시점에 체크를 위해 final을 붙이기
+    private final MemberRepository memberRepository;
+
+//    //생성자 주입 //MemberRepository에 의존한다는 것을 알 수 있다.
+//    @Autowired
+//    public MemberService(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+//
+//    //필드 주입 //테스트할 경우 바꿔줄 수 없다.
+//    @Autowired
+//    private MemberRepository memberRepository;
+//
+//    //세터 주입 //동작 중간에 누군가가 바꿀 위험이 있다.
+//    @Autowired
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+
+    /**
+     * 회원 가입
+     */
+    @Transactional  //안에 선언된게 우선권을 가짐
+    public Long join(Member member) {
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId(); //member를 생성할 때 영속성 컨텍스트가 id(PK)를 생성하므로 id는 항상 보장이 된다.
+    }
+
+    //동일한 이름은 회원가입을 막는 로직
+    // 참고) 이름을 찾아서 0보다 큰지 확인하는 로직도 가능
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());   // 참고) 멀티스레드를 고려하여 유니크제약조건을 사용하면 좋다.
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+
+    }
+
+    //회원 전체 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    //회원 한명 이름으로 조회
+    public Member findMember(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
